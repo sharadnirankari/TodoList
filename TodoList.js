5ec92ffe95a2549180cf10b80b5464d88afd7d45
Sharad Nirankari <63394059+sharadnirankari@users.noreply.github.com>
2024-01-08 16:03:26 +0530

    Day 1

---
+++ TodoList.js
@@ -0,0 +1,73 @@
+// TodoList.js
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { id: Date.now(), text: newTodo }]);
+      setNewTodo("");
+    }
+  };
+
+  const removeTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <View style={{ flex: 1, padding: 20, marginTop: 40 }}>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 10,
+          padding: 5,
+        }}
+        placeholder="Enter a new todo"
+        value={newTodo}
+        onChangeText={(text) => setNewTodo(text)}
+      />
+      <TouchableOpacity
+        onPress={addTodo}
+        style={{ padding: 10, backgroundColor: "blue", alignItems: "center" }}
+      >
+        <Text style={{ color: "white" }}>Add Todo</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: 10,
+            }}
+          >
+            <Text>{item.text}</Text>
+            <TouchableOpacity
+              onPress={() => removeTodo(item.id)}
+              style={{ backgroundColor: "red", padding: 5 }}
+            >
+              <Text style={{ color: "white" }}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default TodoList;
